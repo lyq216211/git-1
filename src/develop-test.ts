4ce7cb55317d5de25a3e692c24ef456dfbecbeff
lyq216211 <1322677782@qq.com>
2024-05-26 10:39:25 +0800

    test2

--- src/develop-test.ts
+++ src/develop-test.ts
@@ -1,1 +1,11 @@
-develop / test
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/react-tours-project": "https://course-api.com",
+    },
+  },
+});
+
+const a = 5641213;
